c83c30e2428722587a4d5585406ed3fddf90e4bf
Zephydoestech <eraldarryll@gmail.com>
2026-01-08 13:35:26 +0800

    Added metro config

--- realease-mobile/tailwind.config.js
+++ realease-mobile/tailwind.config.js
@@ -1,9 +1,11 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  // NOTE: Content must match your folder structure exactly
   content: [
     "./app/**/*.{js,jsx,ts,tsx}", 
     "./components/**/*.{js,jsx,ts,tsx}"
   ],
+  presets: [require("nativewind/preset")], // <--- THIS LINE IS CRITICAL FOR V4
   theme: {
     extend: {},
   },
